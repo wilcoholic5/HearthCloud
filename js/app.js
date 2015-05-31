f51bf22c6f869f3084fa844a8642ee03c3e86e39
cesar <cesar.flores3@live.com>
2015-05-31 12:03:52 -0700

    Added Scripts and nodejs requirements

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngSanitize']);
 app.controller('myCtrl', function($scope) {
     $scope.conn = new WebSocket('ws://localhost:1337');
 
     $scope.conn.onopen = (function (event) {
         console.log('connected')
     });
+
+    $scope.showContent = function($fileContent){
+        console.log('running')
+        $scope.content = $fileContent;
+    };
 
     $scope.conn.onmessage = (function (data) {
         console.log(data.data)
@@ -16,15 +21,58 @@ app.controller('myCtrl', function($scope) {
         //$scope.comments = cardBlock.comments;
         $scope.$apply(function(){
             $scope.result = cardBlock;
+            var words = [];
+            cardBlock.comments.forEach(function (entry) {
+                for (var i = 0; i < entry[1]; i++) {
+                    words.push(entry[0])
+                }
+            });
+            update(words)
+
+            $scope.result.fileLoc = "../cards/Polarity/" + $scope.result.cardName + ".txt";
+            $scope.readFile($scope.result.fileLoc);
+            $scope.result.comments = words;
+
         });
         console.log(cardBlock)
     });
 
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+    $scope.clean = (function (html) {
+        return function(html){
+            return $scope.trustAsHtml(html);
+        }
+    })
 
     $scope.findCard = (function () {
         console.log($scope.name)
         $scope.conn.send($scope.name);
     });
+
+
+    $scope.readFile = (function(fileLoc) {
+        $.get( fileLoc, function( data ) {
+            $( "body" )
+                .append( "Name: " ) // John
+                .append( "Time: "); //  2pm
+        }, "json" );
+    });
 });
+
+function draw(words) {
+    d3.select("body").append("svg")
+        .attr("width", 300)
+        .attr("height", 300)
+        .append("g")
+        .attr("transform", "translate(150,150)")
+        .selectAll("text")
+        .data(words)
+        .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+}
---
+++ scripts/PingCardPages.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import urllib2
+import time
+import re
+
+i = 3000
+cards = []
+for num in range(1, i):
+		webpage = urllib2.Request('http://www.hearthhead.com/card='+str(num))
+		try:
+			contents = urllib2.urlopen(webpage)
+		except urllib2.HTTPError as e:
+			if e.code == 404:
+				print 'card ' + str(num) + ' does not exist'
+				continue
+		print 'adding ' + str(num)
+		cards.append(num)
+		
+print cards
+#soup = BeautifulSoup(browser.page_source)
+#
+#print soup.prettify()
